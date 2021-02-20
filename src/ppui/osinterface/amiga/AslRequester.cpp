946713ab339c9900e60dc06d0e82f7ee0f602664
capehill <juha.niemimaki@gmail.com>
2021-02-20 13:57:06 +0200

    Store filepaths in dictionary using dialog name as key

--- src/ppui/osinterface/amiga/AslRequester.cpp
+++ src/ppui/osinterface/amiga/AslRequester.cpp
@@ -1,7 +1,7 @@
 /*
  *  ppui/osinterface/amiga/AslRequester.cpp
  *
- *  Copyright 2017 Juha Niemimaki
+ *  Copyright 2017-2021 Juha Niemimaki
  *
  *  This file is part of Milkytracker.
  *
@@ -26,6 +26,8 @@
 
 #include "AslRequester.h"
 
+#include "Dictionary.h"
+
 #include <stdio.h>
 
 #include <proto/exec.h>
@@ -33,37 +35,41 @@
 #include <proto/dos.h>
 
 struct AslIFace *IAsl;
+struct Library *AslBase;
 
 static const int maxPath = 255;
+static char defaultPath[maxPath];
 
-static char pathBuffer[maxPath];
+static PPDictionary pathDictionary;
 
 static void GetCurrentPath()
 {
-    if (strlen(pathBuffer) == 0) {
+    if (strlen(defaultPath) == 0) {
         BPTR lock = IDOS->GetCurrentDir();
-        int32 success = IDOS->NameFromLock(lock, pathBuffer, sizeof(pathBuffer));
+        int32 success = IDOS->NameFromLock(lock, defaultPath, sizeof(defaultPath));
 
         if (success) {
-            //printf("Initialized to '%s'\n", pathBuffer);
+            //printf("Initialized to '%s'\n", defaultPath);
         } else {
             puts("Failed to get current dir name, use PROGDIR:");
-            strncpy(pathBuffer, "PROGDIR:", sizeof(pathBuffer));
+            strncpy(defaultPath, "PROGDIR:", sizeof(defaultPath));
         }
     } else {
-        //printf("Use known path '%s'\n", pathBuffer);
+        //printf("Use known path '%s'\n", defaultPath);
     }
 }
 
-static PPSystemString GetFileNameFromRequester(struct FileRequester *req)
+static PPSystemString GetFileNameFromRequester(struct FileRequester *req, CONST_STRPTR key)
 {
     char buffer[maxPath];
     PPSystemString fileName = "";
 
     if (strlen(req->fr_Drawer) < sizeof(buffer)) {
-
         strncpy(buffer, req->fr_Drawer, sizeof(buffer));
-        strncpy(pathBuffer, req->fr_Drawer, sizeof(pathBuffer));
+
+        //printf("key %s, data %s\n", key, req->fr_Drawer);
+
+        pathDictionary.store(key, req->fr_Drawer);
 
         int32 success = IDOS->AddPart(buffer, req->fr_File, sizeof(buffer));
 
@@ -74,9 +80,8 @@ static PPSystemString GetFileNameFromRequester(struct FileRequester *req)
         }
 
         //printf("%s\n", fileName.getStrBuffer());
-
     } else {
-        printf("Path is too long (limit %ld)\n", sizeof(buffer));
+        printf("Path is too long (limit %ld chars)\n", sizeof(buffer));
     }
 
     return fileName;
@@ -86,60 +91,88 @@ struct Window* getNativeWindow(void);
 
 static struct FileRequester *CreateRequester(CONST_STRPTR title, bool saveMode, CONST_STRPTR name)
 {
+    const char* path = NULL;
+
+    PPDictionaryKey* key = pathDictionary.restore(title);
+    if (key) {
+         path = key->getStringValue().getStrBuffer();
+         //printf("Restored path for key %s: %s\n", title, path);
+    }
+
+    if (path == NULL) {
+        path = defaultPath;
+    }
+
     struct FileRequester *req = (struct FileRequester *)IAsl->AllocAslRequestTags(
         ASL_FileRequest,
         ASLFR_Window, getNativeWindow(),
         ASLFR_TitleText, title,
-        //ASLFR_PositiveText, "Open file",
         ASLFR_DoSaveMode, saveMode ? TRUE : FALSE,
         ASLFR_SleepWindow, TRUE,
         ASLFR_StayOnTop, TRUE,
         ASLFR_RejectIcons, TRUE,
-        ASLFR_InitialDrawer, pathBuffer,
-        ASLFR_InitialFile, name,
+        ASLFR_InitialDrawer, path,
+        ASLFR_InitialFile, IDOS->FilePart(name),
         TAG_DONE);
 
     return req;
+}
+
+static bool OpenAslLibrary()
+{
+    AslBase = IExec->OpenLibrary(AslName, 53);
+    if (AslBase) {
+         IAsl = (struct AslIFace *)IExec->GetInterface(AslBase, "main", 1, NULL);
+         if (IAsl) {
+             return true;
+         }
+
+         puts("Failed to get ASL interface");
+     }
+
+     printf("Failed to open %s\n", AslName);
+
+     return false;
+}
+
+static void CloseAslLibrary()
+{
+    if (IAsl) {
+        IExec->DropInterface((struct Interface *)IAsl);
+        IAsl = NULL;
+    }
+
+    if (AslBase) {
+        IExec->CloseLibrary(AslBase);
+        AslBase = NULL;
+    }
 }
 
 PPSystemString GetFileName(CONST_STRPTR title, bool saveMode, CONST_STRPTR name)
 {
     PPSystemString fileName = "";
 
-    struct Library *AslBase = IExec->OpenLibrary(AslName, 53);
+    if (OpenAslLibrary()) {
+        GetCurrentPath();
 
-    if (AslBase) {
-        IAsl = (struct AslIFace *)IExec->GetInterface(AslBase, "main", 1, NULL);
+        struct FileRequester *req = CreateRequester(title, saveMode, name);
 
-        if (IAsl) {
-            GetCurrentPath();
+        if (req) {
+            BOOL result = IAsl->AslRequestTags(req, TAG_DONE);
 
-            struct FileRequester *req = CreateRequester(title, saveMode, name);
+            //printf("%d '%s' '%s'\n", b, r->fr_File, r->fr_Drawer);
 
-            if (req) {
-
-                BOOL result = IAsl->AslRequestTags(req, TAG_DONE);
-
-                //printf("%d '%s' '%s'\n", b, r->fr_File, r->fr_Drawer);
-
-                if (result != FALSE) {
-                    fileName = GetFileNameFromRequester(req);
-                }
-
-                IAsl->FreeAslRequest(req);
-            } else {
-                puts("Failed to allocate file requester");
+            if (result != FALSE) {
+                fileName = GetFileNameFromRequester(req, title);
             }
 
-            IExec->DropInterface((struct Interface *)IAsl);
+            IAsl->FreeAslRequest(req);
         } else {
-            puts("Failed to get ASL interface");
+            puts("Failed to allocate file requester");
         }
-
-        IExec->CloseLibrary(AslBase);
-    } else {
-        printf("Failed to open %s\n", AslName);
     }
+
+    CloseAslLibrary();
 
     return fileName;
 }
